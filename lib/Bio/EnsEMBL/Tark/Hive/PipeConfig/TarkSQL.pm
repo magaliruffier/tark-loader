1cdb0053fac94847ce8b2f4bb2388c2c9c70f163
Mark McDowall <mark.mcdowall@gmail.com>
2019-02-21 09:06:56 +0000

    Adding new SQL

--- lib/Bio/EnsEMBL/Tark/Hive/PipeConfig/TarkSQL.pm
+++ lib/Bio/EnsEMBL/Tark/Hive/PipeConfig/TarkSQL.pm
@@ -100,4 +100,56 @@ sub feature_release_count {
   return $sql;
 } ## end sub feature_release_count
 
+
+=head1 _feature_release_template_SQL
+  Description: Default SQL for the gene grouping queries. This is an internal
+               function and should only be accessed through the gene_grouping,
+               gene_grouping_exclusion and gene_grouping_inclusion that will
+               handle the substitution of the WHERE clauses.
+=cut
+
+sub _feature_diff_template_SQL {
+  my ($self) = @_;
+
+  my $sql = (<<'SQL');
+    SELECT
+      COUNT(*)
+    FROM
+      (
+        SELECT feature_id FROM #FEATURE#_release_tag WHERE release_id=?
+      ) AS v0
+      #DIRECTION# JOIN (
+        SELECT feature_id FROM #FEATURE#_release_tag WHERE release_id=?
+      ) AS v1 ON (v0.feature_id=v1.feature_id)
+    WHERE
+      #SET#.feature_id IS NULL
+SQL
+
+  return $sql;
+} ## end sub _feature_release_template_SQL
+
+
+=head2 feature_diff_count
+  Arg [1]    : $feature - string
+  Description: Query for getting counts of all features
+=cut
+
+sub feature_diff_count {
+  my ( $self, $feature, $direction ) = @_;
+
+  my $sql = $self->_feature_release_template_SQL();
+
+  $sql =~ s/#FEATURE#/$feature/g;
+
+  if ( $direction eq 'removed' ) {
+    $sql =~ s/#DIRECTION#/LEFT/g;
+    $sql =~ s/#SET#/v1/g;
+  } else {
+    $sql =~ s/#DIRECTION#/LEFT/g;
+    $sql =~ s/#SET#/v1/g;
+  }
+
+  return $sql;
+} ## end sub feature_diff_count
+
 1;
