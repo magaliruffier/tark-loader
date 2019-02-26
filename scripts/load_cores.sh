e94489db58e54773f8fceef01be50eeafca89ded
Mark McDowall <mark.mcdowall@gmail.com>
2019-02-26 13:35:13 +0000

    Added defining source inclusion and exclusion parameters

--- scripts/load_cores.sh
+++ scripts/load_cores.sh
@@ -24,12 +24,14 @@ ENSDIR=""
 SPECIES="homo_sapiens"
 ASSEMBLY=38
 RELEASE_FROM=76
-RELEASE_TO=${RELEASE_FROM}
-PREVIOUS_RELEASE=${RELEASE_FROM}
+RELEASE_TO=0
+PREVIOUS_RELEASE=0
 TARK_DB="test_tark"
+EXCLUDE_SOURCE=""
+INCLUDE_SOURCE=""
 verbose=0
 
-while getopts "h?:d:s:a:q:r:p:t:" opt; do
+while getopts "h?:d:s:a:e:i:q:r:p:t:" opt; do
     case "${opt}" in
     h|\?)
         echo "Loader for importing ensembl core dbs into the Tark db."
@@ -43,6 +45,10 @@ while getopts "h?:d:s:a:q:r:p:t:" opt; do
     a)  ASSEMBLY=$OPTARG
         ;;
     d)  ENSDIR=$OPTARG
+        ;;
+    e)  EXCLUDE_SOURCE=$OPTARG
+        ;;
+    i)  INCLUDE_SOURCE=$OPTARG
         ;;
     q)  RELEASE_FROM=$OPTARG
         ;;
@@ -68,6 +74,33 @@ then
   exit 0
 fi
 
+if [ $RELEASE_TO = 0 ]
+then
+  RELEASE_TO=$RELEASE_FROM
+fi
+
+if [ $PREVIOUS_RELEASE = 0 ]
+then
+  PREVIOUS_RELEASE=$RELEASE_FROM
+fi
+
+ESOURCE=${#EXCLUDE_SOURCE}
+ISOURCE=${#INCLUDE_SOURCE}
+if [ "$ESOURCE" -gt "0" && "$ISOURCE" -gt "0" ]
+then
+  echo "Please specify only -e OR -i."
+fi
+
+SOURCE=""
+if [ "$ESOURCE" -gt "0" ]
+then
+  SOURCE=" --exclude_source '${EXCLUDE_SOURCE}'"
+fi
+if [ "$ISOURCE" -gt "0" ]
+then
+  SOURCE=" --include_source '${INCLUDE_SOURCE}'"
+fi
+
 eval $(mysql-ens-tark-dev-1-ensrw details env_TARK_)
 eval $(mysql-ens-hive-prod-2-ensrw details env_HIVE_)
 
@@ -84,7 +117,7 @@ do
 
   echo "Loading ${SPECIES}_core_${RELEASE}_${ASSEMBLY}, PREVIOUS_RELEASE was ${PREVIOUS_RELEASE}"
 
-  perl -Ilocal/lib/perl5 ${ENSDIR}/ensembl-hive/scripts/init_pipeline.pl Bio::EnsEMBL::Tark::Hive::PipeConfig::TarkLoader_conf   --tark_host ${TARK_HOST}   --tark_port ${TARK_PORT}   --tark_user ${TARK_USER}   --tark_pass ${TARK_PASS}   --tark_db ${TARK_DB}   --core_host ${CORE_HOST}   --core_port ${CORE_PORT}   --core_user ${CORE_USER}   --core_pass ''   --core_dbname ${CORE_DB}   --host ${HIVE_HOST}   --port ${HIVE_PORT}   --user ${HIVE_USER}   --password ${HIVE_PASS}   --pipeline_name ${HIVE_DB_NAME}   --species ${SPECIES}   --tag_block release   --tag_shortname ${RELEASE}   --tag_description "Ensembl release ${RELEASE}"   --tag_feature_type all   --tag_version 1   --block_size 1000 --report ${PWD}/loading_report_${RELEASE}.json --tag_previous_shortname ${PREVIOUS_RELEASE}
+  perl -Ilocal/lib/perl5 ${ENSDIR}/ensembl-hive/scripts/init_pipeline.pl Bio::EnsEMBL::Tark::Hive::PipeConfig::TarkLoader_conf   --tark_host ${TARK_HOST}   --tark_port ${TARK_PORT}   --tark_user ${TARK_USER}   --tark_pass ${TARK_PASS}   --tark_db ${TARK_DB}   --core_host ${CORE_HOST}   --core_port ${CORE_PORT}   --core_user ${CORE_USER}   --core_pass ''   --core_dbname ${CORE_DB}   --host ${HIVE_HOST}   --port ${HIVE_PORT}   --user ${HIVE_USER}   --password ${HIVE_PASS}   --pipeline_name ${HIVE_DB_NAME}   --species ${SPECIES}   --tag_block release   --tag_shortname ${RELEASE}   --tag_description "Ensembl release ${RELEASE}"   --tag_feature_type all   --tag_version 1   --block_size 1000 --report ${PWD}/loading_report_${RELEASE}.json --tag_previous_shortname ${PREVIOUS_RELEASE}  ${SOURCE}
 
   perl -Ilocal/lib/perl5 ${ENSDIR}/ensembl-hive/scripts/beekeeper.pl -url mysql://${HIVE_USER}:${HIVE_PASS}@${HIVE_HOST}:${HIVE_PORT}/${HIVE_DB} -loop
 
