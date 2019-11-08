40a6a7deb9e2ea36d55ef149116ca00c4e6cd11b
premanand17 <prem.apa@gmail.com>
2019-11-08 11:13:53 +0000

    script to clean lrg

--- docs/lrg_clean.sql
+++ docs/lrg_clean.sql
@@ -1,22 +1,22 @@
+DELETE from exon_release_tag where feature_id in (SELECT exon_id from exon e where e.stable_id like "LRG%");
+DELETE from exon_transcript  where exon_id in (SELECT exon_id from exon  e where e.stable_id like "LRG%");
+DELETE from exon where stable_id like "LRG%";
 
-select count(*) from exon_release_tag ert inner join exon e on e.exon_id=ert.feature_id  where e.stable_id like "LRG%";
-select count(*) from exon_transcript et inner join exon e on et.exon_id=e.exon_id  where e.stable_id like "LRG%";
-select count(*) from exon where stable_id like "LRG%";
+DELETE from gene_release_tag where feature_id in(SELECT gene_id from gene g where g.stable_id like "LRG%");
 
-select count(*) from gene_release_tag grt inner join gene g on g.gene_id=grt.feature_id  where g.stable_id like "LRG%";
-select count(*) from gene where stable_id like "LRG%";
+DELETE from transcript_release_tag where feature_id in (SELECT transcript_id from transcript t  where t.stable_id like "LRG%");
+DELETE from transcript_gene where transcript_id in(SELECT transcript_id  from transcript t where t.stable_id like "LRG%");
 
+DELETE from translation_release_tag where feature_id in (SELECT translation_id from translation t where t.stable_id like "LRG%");
+DELETE from translation_transcript where translation_id in (SELECT translation_id from translation t  where t.stable_id like "LRG%");
 
+truncate gene_names;
 
-select count(*) from transcript_release_tag trt inner join transcript t on t.transcript_id=trt.feature_id  where t.stable_id like "LRG%";
-select count(*) from transcript_gene tg inner join transcript t on tg.transcript_id=t.transcript_id  where t.stable_id like "LRG%";
-select count(*) from transcript where stable_id like "LRG%";
-
-
-select count(*) from translation_release_tag trt inner join translation t on t.translation_id=trt.feature_id  where t.stable_id like "LRG%";
-select count(*) from translation_transcript tg inner join transcript t on tg.transcript_id=t.transcript_id  where t.stable_id like "LRG%";
-select count(*) from translation where stable_id like "LRG%";
+DELETE from gene where stable_id like "LRG%";
+DELETE from transcript where stable_id like "LRG%";
+DELETE from translation where stable_id like "LRG%";
 
 
-select * from release_set where description like "%LRG%";
+DELETE from release_set where description like "%LRG%";
+
 
